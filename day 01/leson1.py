f243e54806630ebe524905b3f78f437d811a5e93
DAVITI190 <tugushidaviti86@gmail.com>
2024-10-08 19:52:13 +0400

    first commit

---
+++ day 01/leson1.py
@@ -0,0 +1,16 @@
+name = "davit"
+surname ="tugushi"
+age = 15
+
+height = 170
+
+low = False
+
+high = True
+
+print(name)
+
+
+print(surname + "age 15")
+
+print(name + str(height) + str(low))
---
+++ day 01/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+str(height) = 170
