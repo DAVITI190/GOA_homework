f2906e1f578452973155bbead6cc55dda91874f9
DAVITI190 <tugushidaviti86@gmail.com>
2024-10-30 00:32:33 +0400

    commit

---
+++ day 06/classwork/leson6.py
@@ -0,0 +1,122 @@
+#gamokleba
+
+x = 10
+x -= 3
+print(x)
+
+x = 10
+x -=7
+print(x)
+
+x = 7
+x -=6
+print(x)
+
+x = 6
+x -=6
+print(x)
+
+x = 8
+x -=6
+print(x)
+
+#gamravleba
+
+x = 10
+x *= 3
+print(x)
+
+x = 2
+x *= 2
+print(x)
+
+x = 2
+x *= 4
+print(x)
+
+x = 4
+x *= 4
+print(x)
+
+x = 8
+x *= 2
+print(x)
+
+#mimateba
+
+x = 4
+x +=3
+print(x)
+
+x = 8
+x +=4
+print(x)
+
+x = 3
+x += 3
+print(x)
+
+x = 5
+x +=5
+print(x)
+
+x = 10
+x +=2
+print(x)
+
+#gayopa
+
+x = 4
+x /=2
+print(x)
+
+x = 8
+x /=2
+print(x)
+
+x = 10
+x /= 5
+print(x)
+
+x = 6
+x /=3
+print(x)
+
+x = 20
+x /=2
+print(x)
+
+
+
+
+
+
+print "Hello World"
+print("variable")
+print("Hello World")
+    print("This is an indented line")
+print("undeclared_variable")
+class = 5
+print(class)
+print("Hello World")
+x = 10
+y = 5
+print(x + y)
+def (my_function):
+print(Missing indentation in function)
+1var = 10
+print(1var)
+a, b = 10
+print(a. b)
+print("Unclosed string")
+print("Escaped quote: \")
+True = 5
+print(True)
+# Missing comment symbol
+This is a comment
+if True
+print("No colon in if statement")
+def add(a. b):
+return a + b
+variable
+print("Empty statement after variable declaration")
