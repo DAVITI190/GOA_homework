4ae2d0134dc101ae5f23ac19156d24b9b871f2ab
DAVITI190 <tugushidaviti86@gmail.com>
2024-11-05 23:10:00 +0400

    commit

---
+++ day 010/classwork/leson.py
@@ -0,0 +1,56 @@
+
+num1 = int(input("შეიყვანეთ პირველი რიცხვი: "))
+num2 = int(input("შეიყვანეთ მეორე რიცხვი: "))
+num3 = int(input("შეიყვანეთ მესამე რიცხვი: "))
+
+print(int(num1) + " " + int(num2) + " " + int(num3))
+
+
+
+#მომხმარებლის წონისა და სიმაღლის მოთხოვნა
+weight = float(input("შეიყვანეთ თქვენი წონა (კგ): "))
+height = float(input("შეიყვანეთ თქვენი სიმაღლე (მეტრებში): "))
+
+bmi = weight / (height ** 2)
+
+
+
+
+print(5 > 4)
+print(5 > 2)
+print(4 > 1)
+print(2 > 1)
+print(1 > 0)
+
+print(4 < 5)
+print(5 < 6)
+print(6 < 7)
+print(7 < 8)
+print(8 < 9)
+
+print(4 >= 2)
+print(5 >= 3)
+print(6 >= 5)
+print(3 >= 1)
+print(7 >= 6)
+
+print(4 <= 5)
+print(5 <= 6)
+print(6 <= 7)
+print(7 <= 8)
+print(8 <= 9)
+
+print(8 != 9)
+print(7 != 6)
+print(6 != 9)
+print(2 != 4)
+print(3 != 5)
+
+print(10 == 10)
+print(9 == 9)
+print(8 == 8)
+print(7 == 7)
+print(6 == 6)
+
+
+
