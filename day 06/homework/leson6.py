2f8f7b3599c2d14b77e7d3f9a91b4a331c2a8da3
DAVITI190 <tugushidaviti86@gmail.com>
2024-11-08 21:01:24 +0400

    commit

---
+++ day 06/homework/leson6.py
@@ -0,0 +1,54 @@
+number = 15
+# +
+number = number + 29
+
+number = number + 33
+
+number = number + 44
+
+number = number + 75
+
+# -
+number = number - 190
+
+number = number - 888
+
+number = number - 556
+
+number = number - 231
+
+# *
+number = number * 324
+
+number = number * 765
+
+number = number * 984
+
+number = number * 413
+
+# /
+number = number / 12
+
+number = number / 65
+
+number = number / 45
+
+number = number / 14
+
+# %
+number = number % 87
+
+number = number % 74
+
+number = number % 99
+
+number = number % 44
+
+# **
+number = number ** 86
+
+number = number ** 52
+
+number = number ** 999
+
+number = number ** 1000
